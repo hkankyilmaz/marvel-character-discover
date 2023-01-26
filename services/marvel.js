ed02da84c67c0720994fa795b65e38e40804e826
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-01-27 00:46:22 +0300

    services

--- services/marvel.js
+++ services/marvel.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-export const loadMore = () => {
-  let offset = 0;
-  axios
-    .get(
-      `https://gateway.marvel.com:443/v1/public/characters?limit=30&offset=${offset}&apikey=${process.env.MARVEL_PUBLIC_KEY}`
-      // {
-      //   params,
-      // }
-    )
-    .then(({ data }) => {
-      console.log(data);
-    });
+export const load = async (offset, params) => {
+  try {
+    const res = await axios.get(
+      `https://gateway.marvel.com:443/v1/public/characters?limit=30&offset=${offset}&apikey=${process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY}`,
+
+      {
+        params,
+      }
+    );
+
+    return res;
+  } catch (error) {
+    throw new Error("Error happened while fetching characters.", error);
+  }
 };
 
-export const fetchCharacter = () => {
+export const fetchCharacter = (id) => {
   axios
     .get(
-      `https://gateway.marvel.com:443/v1/public/characters/${id}?&apikey=${process.env.MARVEL_PUBLIC_KEY}`
+      `https://gateway.marvel.com:443/v1/public/characters/${id}?&apikey=${process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY}`
     )
-    .then(({ data }) => {
-      console.log(data);
-    });
+    .then(({ data }) => {});
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,31 @@
 "use client";
+import React from "react";
+import axios from "axios";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 
 import styles from "./styles.module.scss";
 import "./styles.module.scss";
 
-import { loadMore } from "@/services/marvel";
+import { load } from "@/services/marvel";
 import { fetchCharacter } from "@/services/marvel";
 
 export default function Home() {
-  console.log(process.env.MARVEL_PUBLIC_KEY);
-  loadMore();
+  const [character, setCharacter] = React.useState([]);
+  let offset = Math.floor(Math.random() * 100);
+  console.log(character);
+
+  React.useEffect(() => {
+    load(offset)
+      .then((res) => setCharacter(res.data.data.results))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className={styles.homeContainer}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      {character.map((item) => (
+        <div>amk</div>
+      ))}
     </div>
   );
 }
