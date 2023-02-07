ca8454da0c476edb357f96098e42b16801416a87
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-02-07 16:27:38 +0300

    some fixes

---
+++ components/cardDetail/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+import "./styles.module.scss";
+
+function index() {
+  return <div>index</div>;
+}
+
+export default index;
