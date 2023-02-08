9915c63adde68aa1feed54fa534990fcf0c63796
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-02-08 11:15:48 +0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import axios from "axios";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
 
 import styles from "./styles.module.scss";
 import "./styles.module.scss";
 import Card from "@/components/card";
-import Header from "@/components/header";
 
 import { load } from "@/services/marvel";
-import { fetchCharacter } from "@/services/marvel";
 
 const Home = async () => {
   let offset = Math.floor(Math.random() * 100);
